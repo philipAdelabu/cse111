0d1a83539b1f9f932bd512fe116ae362ca1800a6
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-15 07:17:33 +0100

    All the functions are defined and the necessary files downloaded

--- passwords.py
+++ passwords.py
@@ -1,6 +1,12 @@
 #
 
 
+LOWER=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+UPPER=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+DIGITS=["0","1","2","3","4","5","6","7","8","9"]
+SPECIAL=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", """, """, ",", ".", "<", ">", "?", "/", "`", "~"]
+    
+
 def word_in_file(word, filename, case_sensitive=False):
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
@@ -22,10 +28,7 @@ def word_has_character(word, character_list):
 
 def word_complexity(word):
     complexity = 0
-    LOWER=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    UPPER=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    DIGITS=["0","1","2","3","4","5","6","7","8","9"]
-    SPECIAL=["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", """, """, ",", ".", "<", ">", "?", "/", "`", "~"]
+
     if word_has_character(word, LOWER):
         complexity += 1
     if word_has_character(word, UPPER):
@@ -38,12 +41,33 @@ def word_complexity(word):
 
 
 def password_strength(password, min_length=10, strong_length=16):
-    complexity = word_complexity(password)
+   
 
-    if len(password) == len(min_length):
+    if word_in_file(password, 'wordlist.txt'):
+        print("Password is a dictionary word and is not secure.")
+        return 0
+    
+    if word_in_file(password, 'wordlist.txt', True):
+        print("Password is a commonly used password and is not secure.")
+        return 0
+    
+    if len(password) < len(min_length):
+        print("Password is too short and is not secure.")
+        return 1
         
-
     if len(password) >= len(strong_length):
         print("Password is long, length trumps complexity this is a good password")
-        return 1
-    return 0
+        return 5
+    
+    return word_complexity(password)
+
+
+def main():
+    password = input("Enter a password or q|Q to quit:  ")
+    while password[0].lower() != 'q':
+        print("Password Strength: {0}".format(password_strength(password)))
+        password = input("Enter a password or q|Q to quit:  ")
+
+        
+if __name__ == "__main__":
+    main()
