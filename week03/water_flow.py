4796170af8d51c0f4377dd0d56ee0e4779244011
Philip Adelabu <adebisiphilip@gmail.com>
2025-05-23 21:50:57 +0100

    added the constant at the global scope

--- week03/water_flow.py
+++ week03/water_flow.py
@@ -1,4 +1,8 @@
-water_density = 998.2
+# I ensure that i separated all the major constants and putting them at the global scope of the file.
+
+WATER_DENSITY = 998.2
+ACCELERATION_GRAVITY = 9.80665
+WATER_DYNAMIC_VISCOSITY = 0.0010016
 
 def water_column_height(tower_height, tank_height):
     column_height = tower_height + (3*tank_height/4)
@@ -7,34 +11,32 @@ def water_column_height(tower_height, tank_height):
 
 def pressure_gain_from_water_height(height):
 
-    gravity = 9.80665
-    pressure   = water_density * gravity * height / 1000
+    pressure   = WATER_DENSITY * ACCELERATION_GRAVITY * height / 1000
     return pressure
 
 
 def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
 
-    pressure_loss = ((-1)*friction_factor*pipe_length*water_density*fluid_velocity**2)/(pipe_diameter * 2000)
+    pressure_loss = ((-1)*friction_factor*pipe_length*WATER_DENSITY*fluid_velocity**2)/(pipe_diameter * 2000)
     return pressure_loss
 
 
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
 
-    pressure_loss = (-1)*(0.04)*(water_density*(fluid_velocity**2)*quantity_fittings) / 2000
+    pressure_loss = (-1)*(0.04)*(WATER_DENSITY*(fluid_velocity**2)*quantity_fittings) / 2000
     return pressure_loss 
 
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
 
-    viscosity = 0.0010016
-    reynold = water_density * hydraulic_diameter * fluid_velocity / viscosity
+    reynold = WATER_DENSITY * hydraulic_diameter * fluid_velocity / WATER_DYNAMIC_VISCOSITY
     return reynold
 
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
     k = (.1 + (50/reynolds_number)) * ((larger_diameter/smaller_diameter)**4 - 1)
-    p = (-1) * k * (water_density * (fluid_velocity **2)) / 2000
+    p = (-1) * k * (WATER_DENSITY * (fluid_velocity **2)) / 2000
     return p
 
 
@@ -60,7 +62,7 @@ def main():
     pressure += loss
     loss = pressure_loss_from_fittings(velocity, quantity_angles)
     pressure += loss
-    loss = pressure_loss_from_pipe_reduction(diameter,
+    loss = pressure_loss_from_pipe_reduction(diameter, 
             velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
     pressure += loss
     diameter = HDPE_SDR11_INNER_DIAMETER
